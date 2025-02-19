5ecf455463a97a0fc211c94bcd12088a028a5a33
anupsinghh <itsanupsingh018@gmail.com>
2025-02-20 01:17:50 +0530

    UI

--- src/components/Particle.js
+++ src/components/Particle.js
@@ -23,7 +23,7 @@ function Particle() {
             speed: 0.05,
           },
           size: {
-            value: 1,
+            value: 0,
           },
           opacity: {
             anim: {
