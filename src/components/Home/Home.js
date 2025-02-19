10a1bf65599539cc81b387f6effef363b4a6440b
anupsinghh <itsanupsingh018@gmail.com>
2025-02-20 01:32:26 +0530

    changes in the Navbar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,6 +4,8 @@ import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
+import About from "../About/About";  // Import About component
+import Project from "../Projects/Projects";
 
 function Home() {
   return (
@@ -42,6 +44,8 @@ function Home() {
         </Container>
       </Container>
       <Home2 />
+      <About />  {/* About Section */}
+      <Project /> {/* Project Section */}
     </section>
   );
 }
